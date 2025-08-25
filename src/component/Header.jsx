30d92409546e351887dc2b356dd88ac916da9ac2
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-25 20:12:55 +0530

    feat: add navbar and logo

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 const Header = () => {
-
-}
-export default Header 
+  return (
+    <header>
+      <div className="logo">
+        <img src="logo.png" alt="Food Fire Logo" />
+        <span>Food Fire</span>
+      </div>
+      <div className="nav-links">
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/login">Login</a>
+      </div>
+    </header>
+  );
+};
+export default Header;
