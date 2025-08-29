25e1f67a98cd81b5d0f770854ff925256b5b6721
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-29 22:15:33 +0530

    feat: add body component to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header'
+import Body from './component/Body'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <Header />
+        <Body />
       </div>
       
     </>
