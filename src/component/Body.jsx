60acdf2d251aedb5232dbd25aea1632bda94aeb6
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-29 22:13:29 +0530

    feat:write logic for card

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 const Body = () => {
+   <div className="conatiner">
+    <div className="resturent-card">
+      <img src=""/>
+      <h2>res name</h2>
+      <h3>rating</h3>
+      
 
+    </div>
+   </div>
 }
 export default Body
