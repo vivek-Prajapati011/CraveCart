b361e2073e2fa26c1eef8a13862814e5f3b8041b
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-31 18:17:07 +0530

    fix: remove unwanted css

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -5,9 +5,7 @@ const Body = () => {
   return (
    <div className="conatiner">
     <div className="resturent-card">
-      <img src=""/>
-      <h2>res name</h2>
-      <h3>rating</h3>
+     
       
 
     </div>
