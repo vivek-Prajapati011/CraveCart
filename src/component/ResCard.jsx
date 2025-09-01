5498d7ae2188dd95a7a336d06b3680affb74a02a
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-01 22:39:30 +0530

    feat: setting rescard

--- src/component/ResCard.jsx
+++ src/component/ResCard.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import data from "../data";
 const ResCard = () => {
+    return (
+        <div>
+            <h2>Book your Fav Meal from Top Resturent</h2>
+        </div>
+
+    )
     
 
 }
