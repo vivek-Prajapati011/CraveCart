16c3c2474070a8c74ac13f850b5ea3b89820c85d
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-02 22:15:33 +0530

    feat: add logic to rescard

--- src/component/ResCard.jsx
+++ src/component/ResCard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import data from "../data";
-const ResCard = () => {
+const ResCard = (props) => {
     return (
-        <div>
-            <h2>Book your Fav Meal from Top Resturent</h2>
+        <div className="resturent-card-container">
+            <div className="res-img">
+            <img src={props.image} />
+            </div>
+            <div className="res-info">
+            <h2>{props.name}</h2>
+            <h3>{props.location}</h3>
+            </div>
+            <div className="res-details">
+            <h2>Average Cost{props.price_for_two}</h2>
+            <h3>{props.offer}</h3>
+            </div>
+            <button>Menu</button>
         </div>
 
     )
