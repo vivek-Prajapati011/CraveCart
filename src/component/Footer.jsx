d485df639236b259d52e5e1fc54ca70945cf5099
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-03 19:46:02 +0530

    feat: add legal links for terms and right

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,21 +1,53 @@
 import React from "react";
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            <div className="quick-link">
-                <h2>Quick Links</h2>
-                <ul>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Carrers</a></li>
-                    <li><a href="#">Blogs</a></li>
-                    <li><a href="#">Partner with us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
+  return (
+    <div className="footer-container">
+      <div className="quick-link">
+        <h2>Quick Links</h2>
+        <ul>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Carrers</a>
+          </li>
+          <li>
+            <a href="#">Blogs</a>
+          </li>
+          <li>
+            <a href="#">Partner with us</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
 
-            </div>
+      <div className="Legal-link">
+        <h2>Ligal</h2>
+        <ul>
+          <li>
+            <a href="#">Terms & Conditions</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Cookie Policy</a>
+          </li>
+          <li>
+            <a href="#">Disclaimers</a>
+          </li>
+          <li>
+            <a href="#">FAQs</a>
+          </li>
+        </ul>
+      </div>
+      <div className="social-media-logo">
+        
 
-        </div>
-    )
-
-}
-export default Footer
+      </div>
+    </div>
+  );
+};
+export default Footer;
