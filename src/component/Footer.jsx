0727c1eca8b7bcee4c90dfdebcb37b3ba6435e7c
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-03 19:36:32 +0530

    feat: set quick links for footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="quick-link">
+                <h2>Quick Links</h2>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Carrers</a></li>
+                    <li><a href="#">Blogs</a></li>
+                    <li><a href="#">Partner with us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
 
 }
 export default Footer
