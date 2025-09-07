3205c95243a7f6b27f38dea4250d1c81839267e3
vivek prajapati <vivek011prajapati@gmail.com>
2025-09-07 22:33:09 +0530

    feat: make menu card that diplay menu items

--- src/component/MenuCard.jsx
+++ src/component/MenuCard.jsx
@@ -1,5 +1,23 @@
 import React from "react";
-const MenuCard = () => {
+const MenuCard = ({key, name, price, image, description,rating}) => {
+  return (
+    <div className="menu-card-container">
+        <div className="menu-card">
+            <div className="card-img">
+                <img src={image} alt={name} />
+                <button>ADD</button>
+            </div>
+            <div className="card-info">
+                <h2>{name}</h2>
+                <h2>{price}</h2>
+                <h2>{rating}</h2>
+                <h2>{description}</h2>
+            </div>
+            
 
+        </div>
+
+    </div>
+  )
 }
 export default MenuCard
